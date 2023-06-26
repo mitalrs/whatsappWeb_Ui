22cb415fa3b3108b7d4a763036c586592c02578f
mitalrs <mitalsapkale1999@gmail.com>
2023-06-26 17:47:21 +0530

    feat: :art: added react query and swapi api
    i was added react infiniteQuery but every time for next page call, make a api call two times.

--- src/Components/ChatBox.jsx
+++ src/Components/ChatBox.jsx
@@ -2,110 +2,127 @@ import React from "react";
 import Chatbg from "../assets/whatsapp-bg.png";
 import ChatMessageComponent from "./ChatMessageComponent";
 
-
 function ChatBox() {
-    const messages = [
-      {
-      "type": "sent",
-      "message": "Hi there!",
-      "time": "09:00"
-      },
-      {
-      "type": "recieved",
-      "message": "Hello! How can I assist you?",
-      "time": "09:01"
-      },
-      {
-      "type": "sent",
-      "message": "My name is Mital Sapkale, and I'm a fresher looking for a job in web development.",
-      "time": "09:02"
-      },
-      {
-      "type": "recieved",
-      "message": "Nice to meet you, Mital! What specific field of web development are you interested in?",
-      "time": "09:03"
-      },
-      {
-      "type": "sent",
-      "message": "I'm specifically interested in React development. I find it fascinating and enjoy building interactive user interfaces.",
-      "time": "09:04"
-      },
-      {
-      "type": "recieved",
-      "message": "That's great! React is a popular framework. Do you have any experience with React?",
-      "time": "09:05"
-      },
-      {
-      "type": "sent",
-      "message": "As a fresher, I have been studying React extensively and have completed a few projects on my own to apply my learning.",
-      "time": "09:06"
-      },
-      {
-      "type": "recieved",
-      "message": "That shows your dedication! Could you tell me a bit about your educational background?",
-      "time": "09:07"
-      },
-      {
-      "type": "sent",
-      "message": "Sure! I have a Bachelor's degree in Computer Science with a focus on web development. I have also taken online courses to further enhance my skills.",
-      "time": "09:08"
-      },
-      {
-      "type": "recieved",
-      "message": "Impressive qualifications, Mital! Are you familiar with any other web technologies apart from React?",
-      "time": "09:09"
-      },
-      {
-      "type": "sent",
-      "message": "Yes, I have a good understanding of HTML, CSS, and JavaScript. I have also worked with Node.js and Express.js for server-side development.",
-      "time": "09:10"
-      },
-      {
-      "type": "recieved",
-      "message": "That's a solid skill set! Are you open to working remotely or are you specifically looking for an on-site position?",
-      "time": "09:11"
-      },
-      {
-      "type": "sent",
-      "message": "I am open to both options. However, I believe remote work offers flexibility and enables me to collaborate effectively from any location.",
-      "time": "09:12"
-      },
-      {
-      "type": "recieved",
-      "message": "Understood. We do have some remote positions available. Could you share your portfolio or any projects you've worked on?",
-      "time": "09:13"
-      },
-      {
-      "type": "sent",
-      "message": "Certainly! Here is the link to my portfolio: [insert link]. It showcases the projects I have completed using React and other web technologies.",
-      "time": "09:14"
-      },
-      {
-      "type": "recieved",
-      "message": "Thank you, Mital. I will review your portfolio. Are you available for an interview next week?",
-      "time": "09:15"
-      },
-      {
-      "type": "sent",
-      "message": "Absolutely! I am available for an interview at your convenience. Please let me know the date and time.",
-      "time": "09:16"
-      },
-      {
-      "type": "recieved",
-      "message": "Great! I will get back to you with the details of the interview. Is there anything else you would like to add?",
-      "time": "09:17"
-      },
-      {
-      "type": "sent",
-      "message": "No, that covers everything. Thank you for considering my application. I look forward to hearing from you soon.",
-      "time": "09:18"
-      },
-      {
-      "type": "recieved",
-      "message": "You're welcome, Mital! We appreciate your interest. Have a wonderful day!",
-      "time": "09:19"
-      }
-      ];
+  const messages = [
+    {
+      type: "sent",
+      message: "Hi there!",
+      time: "09:00",
+    },
+    {
+      type: "recieved",
+      message: "Hello! How can I assist you?",
+      time: "09:01",
+    },
+    {
+      type: "sent",
+      message:
+        "My name is Mital Sapkale, and I'm a fresher looking for a job in web development.",
+      time: "09:02",
+    },
+    {
+      type: "recieved",
+      message:
+        "Nice to meet you, Mital! What specific field of web development are you interested in?",
+      time: "09:03",
+    },
+    {
+      type: "sent",
+      message:
+        "I'm specifically interested in React development. I find it fascinating and enjoy building interactive user interfaces.",
+      time: "09:04",
+    },
+    {
+      type: "recieved",
+      message:
+        "That's great! React is a popular framework. Do you have any experience with React?",
+      time: "09:05",
+    },
+    {
+      type: "sent",
+      message:
+        "As a fresher, I have been studying React extensively and have completed a few projects on my own to apply my learning.",
+      time: "09:06",
+    },
+    {
+      type: "recieved",
+      message:
+        "That shows your dedication! Could you tell me a bit about your educational background?",
+      time: "09:07",
+    },
+    {
+      type: "sent",
+      message:
+        "Sure! I have a Bachelor's degree in Computer Science with a focus on web development. I have also taken online courses to further enhance my skills.",
+      time: "09:08",
+    },
+    {
+      type: "recieved",
+      message:
+        "Impressive qualifications, Mital! Are you familiar with any other web technologies apart from React?",
+      time: "09:09",
+    },
+    {
+      type: "sent",
+      message:
+        "Yes, I have a good understanding of HTML, CSS, and JavaScript. I have also worked with Node.js and Express.js for server-side development.",
+      time: "09:10",
+    },
+    {
+      type: "recieved",
+      message:
+        "That's a solid skill set! Are you open to working remotely or are you specifically looking for an on-site position?",
+      time: "09:11",
+    },
+    {
+      type: "sent",
+      message:
+        "I am open to both options. However, I believe remote work offers flexibility and enables me to collaborate effectively from any location.",
+      time: "09:12",
+    },
+    {
+      type: "recieved",
+      message:
+        "Understood. We do have some remote positions available. Could you share your portfolio or any projects you've worked on?",
+      time: "09:13",
+    },
+    {
+      type: "sent",
+      message:
+        "Certainly! Here is the link to my portfolio: [insert link]. It showcases the projects I have completed using React and other web technologies.",
+      time: "09:14",
+    },
+    {
+      type: "recieved",
+      message:
+        "Thank you, Mital. I will review your portfolio. Are you available for an interview next week?",
+      time: "09:15",
+    },
+    {
+      type: "sent",
+      message:
+        "Absolutely! I am available for an interview at your convenience. Please let me know the date and time.",
+      time: "09:16",
+    },
+    {
+      type: "recieved",
+      message:
+        "Great! I will get back to you with the details of the interview. Is there anything else you would like to add?",
+      time: "09:17",
+    },
+    {
+      type: "sent",
+      message:
+        "No, that covers everything. Thank you for considering my application. I look forward to hearing from you soon.",
+      time: "09:18",
+    },
+    {
+      type: "recieved",
+      message:
+        "You're welcome, Mital! We appreciate your interest. Have a wonderful day!",
+      time: "09:19",
+    },
+  ];
 
   return (
     <div
@@ -114,17 +131,16 @@ function ChatBox() {
         backgroundImage: `url(${Chatbg})`,
       }}
     >
-
-     {
-        messages.reverse().map(
-            (message,index)=>{
-                return (
-                    <ChatMessageComponent key={index} type={message.type} message={message.message} time={message.time}/>
-                )
-            }
-        )
-         
-     }
+      {messages.reverse().map((message, index) => {
+        return (
+          <ChatMessageComponent
+            key={index}
+            type={message.type}
+            message={message.message}
+            time={message.time}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,61 +1,105 @@
 import React from "react";
-import ArchivedBar from "./ArchivedBar";
+// import ArchivedBar from "./ArchivedBar";
 import ChatUser from "./ChatUser";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 function ChatList() {
-  const users = [
-    {
-      userName: "userOne",
-      lastMessege: "how's u?",
-      type: "sent",
-      time: "13:09",
-      url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
-    },
-    {
-      userName: "usertwo",
-      lastMessege: "user two",
-      type: "sent",
-      time: "13:09",
-      url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
-    },
-    {
-      userName: "userthree",
-      lastMessege: "user three",
-      type: "recived",
-      time: "13:09",
-      url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
-    },
-    {
-      userName: "userFour",
-      lastMessege: "user five",
-      type: "sent",
-      time: "13:09",
-      url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
-    },
-    {
-      userName: "userFive",
-      lastMessege: "note user",
-      type: "recived",
-      time: "13:09",
-      url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
-    },
-  ];
+  const { fetchNextPage, isLoading, isFetchingNextPage, error, data } =
+    useInfiniteQuery({
+      queryKey: ["usersData"],
+      queryFn: ({ pageParam = "https://swapi.dev/api/people/?page=1" }) => {
+        console.log({pageParam});
+       return fetch(pageParam).then((res) => res.json());
+      },
+      getNextPageParam: (lastPage, allPages) => {
+        console.log(lastPage.next);
+        return lastPage.next;
+      },
+    });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  // console.log("data", data);
+
+  // const users = [
+  //   {l
+  //     userName: "userOne",  
+  //     lastMessege: "how's u?",
+  //     type: "sent",
+  //     time: "13:09",
+  //     url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
+  //   },
+  //   {
+  //     userName: "usertwo",
+  //     lastMessege: "user two",
+  //     type: "sent",
+  //     time: "13:09",
+  //     url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
+  //   },
+  //   {
+  //     userName: "userthree",
+  //     lastMessege: "user three",
+  //     type: "recived",
+  //     time: "13:09",
+  //     url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
+  //   },
+  //   {
+  //     userName: "userFour",
+  //     lastMessege: "user five",
+  //     type: "sent",
+  //     time: "13:09",
+  //     url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
+  //   },
+  //   {
+  //     userName: "userFive",
+  //     lastMessege: "note user",
+  //     type: "recived",
+  //     time: "13:09",
+  //     url: "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg",
+  //   },
+  // ];
+
   return (
     <div className="flex-grow flex flex-col overflow-y-auto">
       {/* <ArchivedBar /> */}
 
-      {users.map((user, index) => {
+      {/* {data?.results?.map((user, index) => {
         return (
           <ChatUser
             key={index}
-            user={user.userName}
-            message={user.lastMessege}
-            type={user.type}
-            time={user.time}
-            img={user.url}
+            user={user.name}
+            message={user.eye_color}
+            type={"recieved"}
+            time={"12:00"}
+            img={"https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg"}
           />
         );
+      })} */}
+
+      {data?.pages?.map((page) => {
+        return page.results?.map((user, index) => {
+          return (
+            <ChatUser
+              key={index}
+              user={user.name}
+              message={user.eye_color}
+              type={"recieved"}
+              time={"12:00"}
+              img={
+                "https://www.unigreet.com/wp-content/uploads/2023/03/Cute-Cat-Whatsapp-Dp-HD-781x1024.jpg"
+              }
+            />
+          );
+        });
       })}
+
+      {isFetchingNextPage ? (
+        "Loading"
+      ) : (
+        <button onClick={fetchNextPage}>load More</button>
+      )}
     </div>
   );
 }
